7ff1d328578621c61b3461dde701dd64adf12270
nishtechno <burhan.sancakli.ogrenci@gmail.com>
2021-03-27 02:11:32 +0300

    test4

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var loadCounter = 0;
-var loaded = function() {
-    loadCounter += 1;
-    if (loadCounter === 2) {
-        $("iframe").attr("height", "500px");
-        $(window).scrollTo(315,0)
-    }
-    console.log($("iframe").attr("height"));
-}
